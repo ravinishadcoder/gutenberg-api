3f5ad7597998c602d057a6ab4d1f277fee21b815
Ravi Nishad <97040275+ravinishadcoder@users.noreply.github.com>
2023-01-30 19:05:21 +0530

    added alignment for title and body

---
+++ src/dropdown.js
@@ -0,0 +1,24 @@
+<SelectControl
+  onBlur={function noRefCheck(){}}
+  onChange={function noRefCheck(){}}
+  onFocus={function noRefCheck(){}}
+  options={[
+    {
+      disabled: true,
+      label: 'Select an Option',
+      value: ''
+    },
+    {
+      label: 'Option A',
+      value: 'a'
+    },
+    {
+      label: 'Option B',
+      value: 'b'
+    },
+    {
+      label: 'Option C',
+      value: 'c'
+    }
+  ]}
+/>
